f5d0302d1ba84f14c9037c4518e9f95e975739f3
student950 <dhanavadaisheta07@gmail.com>
2026-07-21 12:29:26 +0530

    Added SAVE button and display logic to console

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let count = 0
 let countEl = document.getElementById("count-el")
-console.log(countEl)
 function increment() {
     count++
     countEl.innerText = count
+}
+
+function save() {
+    console.log(count)
 }
