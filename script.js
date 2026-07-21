317f09272c646320e2f991721e372acafd4bfcd5
student950 <dhanavadaisheta07@gmail.com>
2026-07-21 17:04:24 +0530

    Added Previois Entries logic

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
 function increment() {
     count++
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
     let previousEntries = count + " - "
     saveEl.textContent += previousEntries
-    console.log(count)
+    countEl.textContent = 0
+    count = 0
 }
