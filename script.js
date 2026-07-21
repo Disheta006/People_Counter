2c7bf99f691e5bcff75597911012522fc5127c33
student950 <dhanavadaisheta07@gmail.com>
2026-07-21 13:12:08 +0530

    Added previous entites section

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let count = 0
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 function increment() {
     count++
     countEl.innerText = count
 }
 
 function save() {
+    let previousEntries = count + " - "
+    saveEl.textContent += previousEntries
     console.log(count)
 }
